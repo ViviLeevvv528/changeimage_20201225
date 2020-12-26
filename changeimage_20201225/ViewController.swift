b1c7ea3ad500b4588ae2ab53e0c42fd96ebef239
vivi <ashin-jo@yahoo.com.tw>
2020-12-27 00:11:00 +0800

    20201226

--- changeimage_20201225/ViewController.swift
+++ changeimage_20201225/ViewController.swift
@@ -6,20 +6,21 @@
 //
 
 import UIKit
-
+// import 濾鏡套件
+import CoreImage.CIFilterBuiltins
 class ViewController: UIViewController {
 
     @IBOutlet weak var naraImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+//切換尺寸功能
     @IBAction func changeimageSize(_ sender: UISegmentedControl){
+//        設定寬300
         let width: CGFloat = 300
         var height: CGFloat = 0
-        
+//設定三個尺寸選項的圖片比例
         if sender.selectedSegmentIndex == 0 {
             height = width
         } else if sender.selectedSegmentIndex == 1 {
@@ -29,6 +30,26 @@ class ViewController: UIViewController {
         }
         naraImage.frame.size = CGSize(width: width, height: height)
     }
+//    連結濾鏡slider設定
+    @IBAction func filerSliderAdjust(_ sender: UISlider) {
+//        將UIImaget轉換CIImage物件
+        let image = UIImage(named: "nara2")
+        let ciImage = CIImage(image: image!)
+//        產生製造濾鏡效果的 CIFilter 物件：呼叫 colorMonochrome() 產生單色濾鏡效果的 CIFilter
+                let filter = CIFilter.colorMonochrome()
+//        設定CIFilter物件
+                filter.inputImage = ciImage
+        filter.color = CIColor.black
+        filter.intensity = sender.value
+
+//        產生加濾鏡效果的圖片,並將它變成 UIImage
+                if let outputCIImage = filter.outputImage {
+                    let filterImage = UIImage(ciImage: outputCIImage)
+                    naraImage.image = filterImage
+                }
+        
+    }
+
     
 }
 
