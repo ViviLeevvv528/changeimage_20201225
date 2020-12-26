2a095f4596d5bff58fd9fe9f2f86ed9eebd45ca7
vivi <ashin-jo@yahoo.com.tw>
2020-12-26 11:13:45 +0800

    20201226

--- changeimage_20201225/ViewController.swift
+++ changeimage_20201225/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var naraImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeimageSize(_ sender: UISegmentedControl){
+        let width: CGFloat = 300
+        var height: CGFloat = 0
+        
+        if sender.selectedSegmentIndex == 0 {
+            height = width
+        } else if sender.selectedSegmentIndex == 1 {
+            height = width / 3 * 4
+        } else {
+            height = width / 9 * 16
+        }
+        naraImage.frame.size = CGSize(width: width, height: height)
+    }
+    
 }
 
